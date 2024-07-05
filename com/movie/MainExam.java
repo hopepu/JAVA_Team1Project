42b4dfce229b47bbec905eb0e12633745d1fab61
10-1\MBC <whdydwo2@gmail.com>
2024-07-05 13:19:08 +0900

    V0.0.0(test용)

---
+++ com/movie/MainExam.java
@@ -0,0 +1,11 @@
+package com.movie;
+
+public class MainExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("MBC Entertainer에 오신것을 환영합니다.");
+
+	}
+
+}
