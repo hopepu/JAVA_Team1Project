8101013e901fcd349074130c0a98eb53adabbbd0
10-1\MBC <whdydwo2@gmail.com>
2024-07-09 09:36:29 +0900

    V1.1.0(모든 팀원의 메서드 연결진행, 1st 견본)

--- com/show/MainExam.java
+++ com/show/MainExam.java
@@ -3,31 +3,35 @@ package com.show;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import com.show.DTO.LoginDTO;
+import com.show.DTO.DramaDTO;
+import com.show.DTO.MemberDTO;
 import com.show.DTO.ReviewDTO;
 import com.show.DTO.ShowDTO;
+import com.show.DTO.VarietyDTO;
 import com.show.SV.LoginSV;
 import com.show.SV.MyPageSV;
+import com.show.SV.SearchListSV;
 import com.show.SV.SearchSV;
 
 public class MainExam {
 
-	public static LoginDTO lSt = new LoginDTO();
+	public static MemberDTO lSt = new MemberDTO();
 	public static Scanner s = new Scanner(System.in);// 단어 입력
 	public static Scanner sL = new Scanner(System.in);// 긴 문장 입력
-	public static ArrayList<LoginDTO> loginDTOs = new ArrayList<LoginDTO>();
+	public static ArrayList<MemberDTO> loginDTOs = new ArrayList<MemberDTO>();
 	public static ArrayList<ReviewDTO> reviewDTOs = new ArrayList<ReviewDTO>();
 	public static ArrayList<ShowDTO> showDTOs = new ArrayList<ShowDTO>();
+	public static ArrayList<DramaDTO> dramaDTOs = new ArrayList<DramaDTO>();
+	public static ArrayList<VarietyDTO> varietyDTOs = new ArrayList<VarietyDTO>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		lSt.setLoginStatus(false);
 		boolean run = true;
 		System.out.println("<오잼>(가제 : 오늘은 무엇이 재밌을까?)에 오신것을 환영합니다.");
-		System.out.println(lSt.getLoginStatus());
 
-		while (run) {			
-				if (lSt.getLoginStatus()) {
+		while (run) {
+				if (!lSt.getLoginStatus()) {
 					System.out.println("아래의 메뉴에서 골라주세요.");
 					System.out.println("1. 찾아보기  |  2. 로그인");
 					System.out.print(">>>");
@@ -35,11 +39,11 @@ public class MainExam {
 					switch (select) {
 					case 1:
 						System.out.println("찾아보기 메뉴로 들어갑니다.");
-						SearchSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
+						SearchSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs, dramaDTOs, varietyDTOs);
 						break;
 					case 2:
 						System.out.println("로그인 메뉴로 들어갑니다.");
-						lSt = LoginSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
+						lSt = LoginSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs,  dramaDTOs, varietyDTOs);
 						break;
 					default:
 						System.out.println("1~2를 골라주세요");
@@ -52,11 +56,11 @@ public class MainExam {
 					switch (select) {
 					case 1:
 						System.out.println("찾아보기 메뉴로 들어갑니다.");
-						SearchSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
+						SearchSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs, dramaDTOs, varietyDTOs);
 						break;
 					case 2:
 						System.out.println("회원정보관리 메뉴로 들어갑니다.");
-						lSt = MyPageSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
+						lSt = MyPageSV.menu(s, loginDTOs, lSt);
 						break;
 					default:
 						System.out.println("1~2를 골라주세요");
